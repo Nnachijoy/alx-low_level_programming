2f6f89a0efbf6a115c8ff252540a38ebab170da7
Nnachijoy <nnachijoychima200@gmail.com>
2022-10-18 22:53:28 -0700

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,5 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c)
 
-/**
- * main - prints putchar
- * Return: always 0
- */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
-}
+#endif
